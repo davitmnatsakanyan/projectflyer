e83b0daaa06cf5e1541e7021e223e8bd0233f937
gevsagh <gevsagh@gmail.com>
2016-03-20 23:46:35 +0400

    running gulp

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@section('content')
+    <h2>Home page</h2>
+    <button class=" btn btn-primary">Button </button>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Project flyer</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
